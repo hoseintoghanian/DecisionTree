4b0c1273e2c9198401875598a864a84349c0f3aa
SaeedFakhrabadi <saeedfakhrabadi8@gmail.com>
2024-02-01 00:55:10 +0330

    delete comments

--- src/Main.java
+++ src/Main.java
@@ -27,34 +27,6 @@ public class Main {
 //		Node root = Dtree.buildTree(feature, 0 , 8878 , 5);
 		System.out.println("Generated decision tree:");
 
-//		// Train-Test split
-//		double[][] X = new double[feature.length][feature[0].length - 1];
-//		double[] Y = new double[feature.length];
-//		for (int i = 0; i < feature.length; i++) {
-//			System.arraycopy(feature[i], 0, X[i], 0, feature[i].length - 1);
-//			Y[i] = feature[i][feature[i].length - 1];
-//		}
-//
-//		int randomState = 41;
-//		double[][] X_train, X_test;
-//		double[] Y_train, Y_test;
-//
-//		// Assuming train_test_split is similar to Python's sklearn.model_selection.train_test_split
-//		trainTestSplit(X, Y, 0.2, randomState, X_train, X_test, Y_train, Y_test);
-//
-//		// Fit the model
-//		DecisionTreeClassifier classifier = new DecisionTreeClassifier(3, 3);
-//		classifier.fit(X_train, Y_train);
-//		classifier.printTree();
-//
-//		// Test the model
-//		double[] Y_pred = classifier.predict(X_test);
-//		System.out.println(Arrays.toString(Y_pred));
-//
-//		// Assuming accuracy_score is similar to Python's sklearn.metrics.accuracy_score
-//		double accuracy = accuracyScore(Y_test, Y_pred);
-//		System.out.println("Accuracy: " + accuracy);
-
         // Example usage with a list of children Nodes
         Node child1 = new Node(new float[]{1.0F, 1.0F, 0.0F , 2.0F , 3.0F , 3.0F});
         Node child2 = new Node(new float[]{1.0F, 1.0F, 1.0F , 5.0F});
